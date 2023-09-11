877a8bdaad13a59f092cc2b87cab1d46da952d97
usamah <hakaro375@gmail.com>
2023-09-11 17:29:35 +0700

    style(app, admin): add explanation

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// Set the view engine to EJS and define the views directory
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// Import route handlers
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// Middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define routes for '/admin' using 'adminData' router from admin.js with destructuring object and other routes using 'shopRoutes'
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+// Handle 404 (Not Found) errors and render a custom view
 app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
+// Start the Express app and listen on port 3000
 app.listen(3000);
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,32 @@
 const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
+const rootDir = require('../util/path'); // Assuming this imports a utility for getting the root directory path
 
-const router = express.Router();
+const router = express.Router(); // Create an Express Router instance
 
-const products = [];
+const products = []; // Initialize an empty array to store products
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
+  // Handle GET requests to '/admin/add-product' route
   res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    //pageTitle and path we output it in head.ejs and navigation.ejs
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product', 
+    formsCSS: true, // A flag to indicate whether to include forms-related CSS (not shown in this code)
+    productCSS: true, // A flag to indicate whether to include product-related CSS (not shown in this code)
+    activeAddProduct: true // A flag to indicate that the "Add Product" link in the navigation is active (not shown in this code)
   });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
+  // Handle POST requests to '/admin/add-product' route
+  products.push({ title: req.body.title }); // Add a new product to the products array based on the title received in the request body
+  res.redirect('/'); // Redirect to the root path ('/') after adding the product
 });
 
+// Export the router and the products array so that they can be used in other parts of the application
 exports.routes = router;
 exports.products = products;
