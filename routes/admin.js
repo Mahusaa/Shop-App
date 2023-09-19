6210d3a0171a7ae22754b73a6e9d2e081facab39
usamah <hakaro375@gmail.com>
2023-09-19 12:11:56 +0700

    feat(routes: admin&shop): add more get method

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,9 @@ const router = express.Router(); // Create an Express Router instance
 // /admin/add-product => GET
 router.get('/add-product', productControllers.getAddProducts);
 
+//admin/products => GET
+router.get("products")
+
 // /admin/add-product => POST
 router.post('/add-product', productControllers.postAddProduct);
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,14 @@ const productControllers = require("../controllers/products")
 const router = express.Router();
 
 
-
+// get "/" page with the controller
 router.get('/', productControllers.getProducts);
+
+router.get("/products");
+
+router.get("/cart");
+
+router.get("/checkout");
+
 
 module.exports = router;
