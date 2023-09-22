89f1d57e1a8cda6fa09355b3623198f8eb2691d7
usamah <hakaro375@gmail.com>
2023-09-22 08:34:32 +0700

    feat(addProduct): add imageUrl, price, and desc

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,13 @@ exports.getAddProducts = (req, res, next) => {
 
 // Handle POST requests to '/admin/add-product' route
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageURL = req.body.imageURL;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageURL, price, description);
   product.save()
-  res.redirect("/");
+  res.redirect("/products");
 };
 
 exports.getProducts = (req, res, next) => {
