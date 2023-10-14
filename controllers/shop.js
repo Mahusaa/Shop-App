69998cfe893be8339a35d3924cc5dddbcacef652
root <root@Lisan-Al-Gaib>
2023-10-14 14:30:42 +0700

    feat(controller): render product-details af search

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,7 +46,10 @@ exports.getOrders = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    console.log(product);
+    res.render("shop/product-details", {
+      product: product, 
+      pageTitle: product.title,
+      path: "/products",
+    })
   })
-  res.redirect("/");
 }
