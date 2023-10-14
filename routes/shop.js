7f9a59f884b161cf14a9495db15b0574517f30f9
root <root@Lisan-Al-Gaib>
2023-10-14 16:15:10 +0700

    feat(controllers): add post routes for add to cart

--- routes/shop.js
+++ routes/shop.js
@@ -7,10 +7,11 @@ const shopControllers = require("../controllers/shop")
 const router = express.Router();
 
 
-// get "/" page with the controller
+// get and post url with the controller
 router.get('/', shopControllers.getIndex);
 router.get("/products", shopControllers.getProducts);
 router.get("/products/:productId", shopControllers.getProduct);
+router.post("/cart", shopControllers.postCart);
 router.get("/cart", shopControllers.getCart);
 router.get("/checkout", shopControllers.getCheckout);
 router.get("/orders", shopControllers.getOrders);
