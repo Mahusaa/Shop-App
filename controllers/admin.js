68e1de541dbb25f5bf9c9bee2880ddc2db3cfba0
root <usamah.hafizh@ui.ac.id>
2023-10-25 08:02:05 +0700

    fix(edit): make post edit product works

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageURL = req.body.imageURL;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageURL, price, description);
+  const product = new Product(null, title, imageURL, price, description);
   product.save(() => {
     res.redirect("/products");
   })
