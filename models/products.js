4b7b9fac4e52884ecd83bf89fa0db2516c0f669c
root <usamah.hafizh@ui.ac.id>
2023-11-04 15:20:42 +0700

    feat(models/products): make fetchALl works using callback

--- models/products.js
+++ models/products.js
@@ -1,26 +1,6 @@
-const path = require("path");
-const fs = require("fs");
-const Cart = require("./cart")
+const db = require("../util/database");
 
-// Define the path to the JSON file where product data is stored
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    "data",
-    "products.json"
-);
 
-// Function to read products from the file and pass them to a callback.
-const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            return cb([]); // If there's an error reading the file, pass an empty array to the callback.
-        } else {
-            cb(JSON.parse(fileContent)); // Parse the file content (assumed to be JSON) and pass it to the callback.
-        }
-    });
-};
-
-// Export a class named Product
 module.exports = class Product {
     constructor(id, title, imageURL, price, description) {
         this.id = id;
@@ -30,66 +10,21 @@ module.exports = class Product {
         this.description = description;
     }
 
-    // Method to save a product to the JSON file
-    save(cb) {
-        getProductsFromFile(products => {
-            if (this.id) {
-                // If the product has an ID, update the existing product
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    if (cb) {
-                        cb();
-                    }
-                });
-            } else {
-                // If the product does not have an ID, generate one and add it as a new product
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), err => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    if (cb) {
-                        cb();
-                    }
-                });
-            }
-        });
+    save() {
     }
 
-    // Method to fetch all products and pass them to a callback
     static fetchAll(cb) {
-        getProductsFromFile(cb);
+        db.query('SELECT * FROM products', (error, results) => {
+            if (error) {
+                console.error('Error fetching products:', error);
+            } else {
+                cb(results.rows)
+            }
+});
     }
 
-    // Method to find a product by its ID and pass it to a callback
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(product => product.id === id);
-            cb(product);
-        });
+    static findById() {
     }
-    // Method to delete product by an ID using post request
-    static deleteById(id, cb){
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if(err){
-                console.log(err);
-                }
-                if(cb){
-                    Cart.deleteProduct(id, product.price, () => {
-                        cb();
-                    });
-                }
-            })
-        })
-    }
+    static deleteById(){
 };
-
+}
